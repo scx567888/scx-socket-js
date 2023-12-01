58a0c4477cd2ff6a062154a96760c3aed2fdd3e4
scx567888 <scx567888@outlook.com>
2023-12-01 20:45:54 +0800

    dev

---
+++ ScxSocketOptions.js
@@ -0,0 +1,43 @@
+class ScxSocketOptions {
+    pingInterval;
+
+    pingTimeout;
+
+    seqIDClearTimeout;
+
+    constructor() {
+        this.pingInterval = 1000 * 5;
+        this.pingTimeout = 1000 * 5;
+        this.seqIDClearTimeout = 1000 * 60 * 10;
+    }
+
+    getPingInterval() {
+        return this.pingInterval;
+    }
+
+    setPingInterval(pingInterval) {
+        this.pingInterval = pingInterval;
+        return this;
+    }
+
+    getPingTimeout() {
+        return this.pingTimeout;
+    }
+
+    setPingTimeout(pingTimeout) {
+        this.pingTimeout = pingTimeout;
+        return this;
+    }
+
+    getSeqIDClearTimeout() {
+        return this.seqIDClearTimeout;
+    }
+
+    setSeqIDClearTimeout(seqIDClearTimeout) {
+        this.seqIDClearTimeout = seqIDClearTimeout;
+        return this;
+    }
+    
+}
+
+export {ScxSocketOptions};
